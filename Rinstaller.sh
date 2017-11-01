efb347bb787dc35a2c797dcdfe00248ef2adda0f
Amit Kohli <mexindian@gmail.com>
2017-11-01 09:50:58 +0000

    Create Rinstaller.sh

---
+++ Rinstaller.sh
@@ -0,0 +1,18 @@
+!/bin/bash
+# Ask the user what package to install
+echo what package should I grab?
+read varname
+
+echo I assume you mean CRAN, but to use github type "g"
+read source
+
+if [ "$source" = "g" ]; then
+        echo --------------------------------
+        echo Installing $varname from GitHub
+        sudo su - -c \\"R -e \"devtools::install_github('$varname')\"\\"
+else
+        echo --------------------------------
+        echo Grabbin $varname from CRAN
+        sudo su - -c \\"R -e \"install.packages('$varname', repos='http://cran.$
+fi
+
